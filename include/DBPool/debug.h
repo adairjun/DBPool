1e33265d6ebcd059aff961a25c25e02d9f59aa08
Xiong Jun <adairjun@gmail.com>
2015-10-21 18:08:14 +0800

    MOD:littlt mod

--- include/DBPool/debug.h
+++ include/DBPool/debug.h
@@ -8,6 +8,7 @@
 
 #include <iostream>
 // 这个BUG_FILE()宏是用来打印错误信息,使用编译器内置宏__FILE__和__LINE__来定位错误位置
+// 这里可不能像Effective C++说的把#define替换成inline函数,否则的话__FILE__将永远指向debug.h,__LINE__将永远是14
 #define BUG_FILE() \
 { \
     std::cerr << " FILE(" << __FILE__ << "), LINE(" << __LINE__ << ")" << std::endl; \
